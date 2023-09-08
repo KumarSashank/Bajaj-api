89a3c7b7d6f81acbf4f8e514be748c0378aa0347
Kumar Sashank <72877934+KumarSashank@users.noreply.github.com>
2023-09-08 16:33:55 +0530

    Add files via upload

---
+++ controllers/bfhl_controller.js
@@ -0,0 +1,61 @@
+module.exports.getreq=async(req,res)=>{
+    console.log('checking from console')
+    res.send({
+        "operation_code":1
+    })
+}
+
+module.exports.postreq=async(req,res)=>{
+    console.log('post')
+    const data=req.body.data;
+    // let num,alphabets,highest_alphabet;
+    //map the data array
+    console.log(data)
+    // data.map((element)=>{
+    //     console.log(element)
+    // });
+
+    const numbers = [];
+    const alphabets = [];
+    let highestAlphabet;
+    let bool=false
+
+    for (const item of data) {
+        if (typeof item === 'string' && item.length === 1) {
+            if (/[0-9]/.test(item)) {
+                // It's a number
+                numbers.push(item);
+            } else if (/[a-zA-Z]/.test(item)) {
+                bool=true
+                // It's an alphabet
+                alphabets.push(item);
+                if (!highestAlphabet || item > highestAlphabet) {
+                    highestAlphabet = item;
+                    
+                }
+            }
+        }
+    }
+    let highest_alphabet
+    if(bool==true){
+        highest_alphabet=[highestAlphabet]
+    }
+    else{
+        highest_alphabet=[]
+    }
+
+    console.log(numbers)
+    console.log(alphabets)
+    // console.log(highest_alphabet)
+    
+    const response_data={
+        "is_success":true,
+        "user_id": "manaswini_surusomayajula_21062003",
+        "email": "manaswini_s@srmap.edu.in",
+        "roll_number":"AP20110020027",
+        "numbers": numbers,
+        "alphabets":alphabets,
+        "highest_alphabet":highest_alphabet
+    }
+    res.send(response_data)
+}
